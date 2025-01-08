f6f102f999e4159e155178be20b5b3f8eefc525d
Gabriel Avrahami <gavrahami50@stuy.edu>
2025-01-07 19:57:36 -0500

    Persistent worker works in that it keeps going and accepts the next client. However, client doesnt exit gracefully is server does ctrl+c

--- persistent_server.c
+++ persistent_server.c
@@ -1,46 +1,50 @@
 #include "pipe_networking.h"
 
-/*static void sighandler(int signo) {
+void eachClient() {
+  printf("1st spot\n");
+  int to_client;
+  int from_client;
+
+  from_client = server_handshake( &to_client );
+  printf("handshake done\n");
+
+  int x = 0;
+  char str[16];
+  while (1) {
+    printf("hi 2nd\n");
+    x = (int) rand() % 100 + 1;
+    sprintf(str, "%d", x);
+    //int errno;
+    //errno = write(to_client, str, 16);
+    write(to_client, str, 16);
+    /*if (errno == -1) {
+      printf("errno %d\n", errno);
+      printf("%s\n", strerror(errno));
+      exit(1);
+    }*/
+    printf("2nd spot\n");
+    sleep(1);
+  }
+  printf("3rd spot\n");
+  close(to_client);
+  close(from_client);
+  printf("4th spot\n");
+}
+
+static void sighandler(int signo) {
   if (signo == SIGINT) {
+    unlink("./toServer");
+    exit(1);
   }
   if (signo == SIGPIPE) {
+    eachClient();
   }
-}*/
+}
 
 int main() {
-  //signal(SIGINT, sighandler);
-  //signal(SIGPIPE, sighandler);
-
+  signal(SIGINT, sighandler);
+  signal(SIGPIPE, sighandler);
   srand(time(NULL));
-
-  //while (1) {
-    printf("1st spot\n");
-    int to_client;
-    int from_client;
-
-    from_client = server_handshake( &to_client );
-    printf("handshake done\n");
-
-    int x = 0;
-    char str[16];
-    while (1) {
-      printf("hi 2nd\n");
-      x = (int) rand() % 100 + 1;
-      sprintf(str, "%d", x);
-      //int errno;
-      //errno = write(to_client, str, 16);
-      write(to_client, str, 16);
-      /*if (errno == -1) {
-        printf("errno %d\n", errno);
-        printf("%s\n", strerror(errno));
-        exit(1);
-      }*/
-      printf("2nd spot\n");
-      sleep(1);
-    }
-    printf("3rd spot\n");
-  //  close(to_client);
-  //  close(from_client);
-  //}
-  printf("4th spot\n");
+  eachClient();
+  return 0;
 }
