de6c699fde034f6669ab7b960c837794a7c2a98b
Gabriel Avrahami <117397869+Gabriel-Avrahami@users.noreply.github.com>
2025-01-08 22:33:07 -0500

    I added a space before the pid and removed the pipes in the sighandler

--- basic_client.c
+++ basic_client.c
@@ -1,24 +1,22 @@
 #include "pipe_networking.h"
 
 static void sighandler(int signo) {
-  if (signo == SIGPIPE) {
-    int pid = getpid();
-    char pp[16];
-    sprintf(pp, "%d", pid);
-    remove(pp);
-    exit(0);
-  }
+  int pid = getpid();
+  char pp[16];
+  sprintf(pp, "%d", pid);
+  remove(pp);
+  exit(0);
 }
 
 int main() {
-  signal(SIGINT, sighandler);
   signal(SIGPIPE, sighandler);
+  signal(SIGINT, sighandler);
   int to_server;
   int from_server;
 
   from_server = client_handshake( &to_server );
 
-  char pid[32] = "my pid is";
+  char pid[32] = "my pid is ";
   char id[16];
   sprintf(id, "%d", getpid());
   strcat(pid, id);
