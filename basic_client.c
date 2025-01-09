db3e240f0ee0edad9778fc5a747ab4bd1606796d
Gabriel Avrahami <117397869+Gabriel-Avrahami@users.noreply.github.com>
2025-01-08 22:15:53 -0500

    Fixed basic_client mostly though not sighandler

--- basic_client.c
+++ basic_client.c
@@ -1,17 +1,22 @@
 #include "pipe_networking.h"
 
+  int pid = getpid();
+  char pp[16];
+  sprintf(pp, "%d", pid);
+  remove(pp);
+
 int main() {
   int to_server;
   int from_server;
 
   from_server = client_handshake( &to_server );
 
+  char pid[32] = "my pid is";
+  strcat(pid, atoi(getpid()));
+  if (write(to_server, pid, strlen(pid)) == -1) {
+    
   char str[16];
-  while (read(from_server, str, 16)) {
-    printf("%s\n", str);
-  }
-  int pid = getpid();
-  char pp[16];
-  sprintf(pp, "%d", pid);
-  remove(pp);
+  read(from_server, str, 16);
+  printf("Returned: %s\n", str);
+  sleep(1);
 }
