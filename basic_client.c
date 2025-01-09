8131120bf0c3049b7cf69359f2d583b563fcaeff
Gabriel Avrahami <117397869+Gabriel-Avrahami@users.noreply.github.com>
2025-01-08 22:18:18 -0500

    The sighandler is in the house

--- basic_client.c
+++ basic_client.c
@@ -1,11 +1,18 @@
 #include "pipe_networking.h"
 
-  int pid = getpid();
-  char pp[16];
-  sprintf(pp, "%d", pid);
-  remove(pp);
+static void sighandler(int signo) {
+  if (signo == SIGPIPE) {
+    int pid = getpid();
+    char pp[16];
+    sprintf(pp, "%d", pid);
+    remove(pp);
+    exit(0);
+  }
+}
 
 int main() {
+  signal(SIGINT, sighandler);
+  signal(SIGPIPE, sighandler);
   int to_server;
   int from_server;
 
@@ -13,7 +20,7 @@ int main() {
 
   char pid[32] = "my pid is";
   strcat(pid, atoi(getpid()));
-  if (write(to_server, pid, strlen(pid)) == -1) {
+  write(to_server, pid, strlen(pid);
     
   char str[16];
   read(from_server, str, 16);
